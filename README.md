61bb0afea14f8ba1e70d50277cf4882cc1ad4674
alsuhr <als495@cornell.edu>
2018-07-24 17:16:31 -0400

    some more explanation

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # atis
+This is the code associated with the paper:
+
+Learning to map context dependent sentences to executable formal queries. Alane Suhr, Srinivasan Iyer, and Yoav Artzi. In NAACL, 2018. [paper](http://alanesuhr.com/atis.pdf) [bib](https://aclanthology.coli.uni-saarland.de/papers/N18-1203/n18-1203.bib)
+
 I will be slowly adding and documenting the code. Please open an issue if you have any questions about the code or getting it to run.
 
 If you want access to the data, code for preprocessing it, and the database to execute the queries, please email me (`suhr@cs.cornell.edu`). ATIS comprises LDC93S5, LDC94S19, and LDC95S26.
--- run.sh
+++ run.sh
@@ -13,3 +13,13 @@ python3 run.py --raw_train_filename="../atis_data/data/resplit/processed/train_w
                --reweight_batch=1 \
                --train=1 
 
+# Explanation of parameters (S# indicates where it's described in the paper):
+# anonymize: whether to apply anonymization preprocessing to the input/output pairs (S6).
+# anonymization_scoring: whether to score anonymized tokens separately from regular output tokens using attention (S6).
+# use_snippets: whether to use the query segment copier (S4.4).
+# state_positional_embeddings: whether to add positional embeddings to the input sequence hidden states during attention computations (S4.3)
+# snippet_age_embedding: whether to use age embeddings for the segments that are copied (S4.4)
+# discourse_level_lstm: whether to use the turn-level encoder (S4.4)
+# interaction_level: whether to construct a computation graph for the entire interaction (S4.3; should be set to the same value as discourse_level_lstm)
+# reweight_batch: whether to reweight the gradients based on interaction length (S5)
+
